5378d8c78859f9b33d7a43cc10f322da1f3f8811
Faris Abuali <fabuali@restaurant365.com>
2022-11-12 00:19:13 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import YouTubeForm from './components/YouTubeForm'
+import "./styles/App.css";
+
+const App = () => {
+  return (
+    <div className='App'>
+      <YouTubeForm />
+    </div>
+  )
+}
+
+export default App
